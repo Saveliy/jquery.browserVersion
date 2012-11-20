4d0aefed5a7f1495eae261e52490d3dff96e6923
Saveliy <svet-noch@yandex.ru>
2012-11-21 03:09:02 +0400

    Создан плагин. Версия 1.0. Добавлено корректное определение версии браузера для Chrome.

---
+++ jquery.browserDetected.js
@@ -0,0 +1,35 @@
+/**
+ * @author: Saveliy Bondarenko
+ * @date: 21.11.12, 2:44
+ * version 1.0
+ */
+
+(function($){
+	jQuery.fn.browserDetected = function(){
+		var item = $(this);
+
+		var name;
+		var browserVersion = $.browser.version;
+
+		if ($.browser.msie)
+			name = 'ie';
+		else if ($.browser.mozilla)
+			name = 'ff';
+		else if ($.browser.opera)
+			name = 'op';
+		else if ($.browser.webkit && window.navigator.userAgent.indexOf('Chrome') > 0)
+			name = 'ch';
+		else if ($.browser.safari)
+			name = 'sf';
+
+		// Переопределение версии браузера для Chrome.
+		if (name == 'ch') {
+			var regexp=/Chrome\/([0-9]+)/;
+			browserVersion = regexp.exec(window.navigator.userAgent)[1];
+		}
+
+		$(item).addClass(name + ' ' + name + parseInt(browserVersion));
+
+		return item;
+	};
+})(jQuery);
